b7c71ee1826cda45ebffddb4b65b264b5896abe2
Bekhzod <stx.webdev@gmail.com>
2023-11-27 19:17:41 +0500

    create <BaseCheckbox /> component with label prop

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,16 @@
+<template>
+  <input class="field" />
+  <label v-if="label">{{ label }}</label>
+</template>
+
+<script>
+export default {
+  name: 'BaseCheckbox',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
