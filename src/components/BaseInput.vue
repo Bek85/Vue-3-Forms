34599f9af16d0d6482db900eea1c74b073b58a60
Bekhzod <stx.webdev@gmail.com>
2023-11-27 18:09:14 +0500

    add label prop to <BaseInput /> component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <label>{{ label }}</label>
+  <input :placeholder="label" class="field" />
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+
+  }
+}
+</script>
