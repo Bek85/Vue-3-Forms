83843a6d0316633e801f8b87775dd4b671abe035
Bekhzod <stx.webdev@gmail.com>
2023-11-27 19:29:21 +0500

    create <BaseCheckbox /> component with label prop

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,16 @@
+<template>
+  <input type="radio" />
+  <label v-if="label">{{ label }}</label>
+</template>
+
+<script>
+export default {
+  name: 'BaseRadio',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
